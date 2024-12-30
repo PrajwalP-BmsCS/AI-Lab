52b8bd1c64fec286e938e645d3d7fe800d6d48be
PrajwalP-BmsCS <153201496+PrajwalP-BmsCS@users.noreply.github.com>
2024-12-30 19:39:09 +0530

    report

--- Week7/entail.py
+++ Week7/entail.py
@@ -10,11 +10,8 @@ def is_entailment(kb, query):
    
     # Simplify the combined KB to CNF
     simplified_kb = simplify_logic(kb_with_negated_query, form="cnf")
-   
     # If the simplified KB evaluates to False, the query is entailed
     return simplified_kb == False
-
-
 
 
 # Define a larger Knowledge Base
